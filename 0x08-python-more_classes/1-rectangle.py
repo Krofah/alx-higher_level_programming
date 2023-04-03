229ea93cfd1f443ca7fc69238aaec43d8dd18f28
Krofah <krofahamponsah@gmail.com>
2023-04-03 14:09:10 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
+"""Author - Krofah"""
 """
 A module with a Rectangle that is empty
 """
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = 0
-        self.__height = 0
         self.width = width
         self.height = height
-
-    def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})"
-
-    def __str__(self):
-        return f"Rectangle with width={self.width} and height={self.height}"
-
-    def get_width(self):
+    
+    @property
+    def width(self):
         return self.__width
-
-    def set_width(self, value):
+    
+    @width.setter
+    def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    def get_height(self):
+    
+    @property
+    def height(self):
         return self.__height
-
-    def set_height(self, value):
+    
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    width = property(get_width, set_width)
-    height = property(get_height, set_height)
 
